5dac255cb32d71f60715244200045ab6a9b1aae6
WillC-E <b5020855@my.shu.ac.uk>
2019-04-16 15:06:53 +0100

    User can create a profile if they are logged in. If they are not logged in the option is not avaliable.

--- reviewApp/app/controllers/profiles_controller.rb
+++ reviewApp/app/controllers/profiles_controller.rb
@@ -1,16 +1,29 @@
 class ProfilesController < ApplicationController
   def index
+    @profiles = Profile.all
   end
 
   def new
+    @profile = Profile.new
   end
 
   def show
+    @profile = Profile.find(params[:id])
   end
 
   def create
+    @profile = Profile.new(profile_params)
+    if @profile.save
+      redirect_to profile_path(@profile)
+    else
+      render 'new'
+    end
   end
 
   def edit
+  end
+  private
+  def profile_params
+    params.require(:profile).permit(:fullName, :dob, :address, :city, :country)
   end
 end
